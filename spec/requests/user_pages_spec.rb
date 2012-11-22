4c4bf2b3ae5b39b80991459a1caa199b8f1e67a0
Adi <oktaviandi@gmail.com>
2012-11-22 14:51:26 +0700

    Add test cases

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,9 +21,17 @@ describe "UserPages" do
   describe "signup" do
     before {visit signup_path}
     let(:submit) {"Create my account"}
+    
     describe "with invalid information" do
       it "should not create a user" do
         expect {click_button submit}.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before {click_button submit}
+
+        it {should have_selector('title', text: 'Sign Up')}
+        it {should have_content('error')}
       end
     end
 
@@ -38,7 +46,14 @@ describe "UserPages" do
       it "should create a user do" do
         expect {click_button submit}.to change(User, :count).by(1)
       end
-    end
 
+      describe "after saving the user" do
+        before {click_button submit}
+        let (:user) {User.find_by_email("user@example.com")}
+
+        it {should have_selector('h1', text: user.name)}
+        it {should have_content('Welcome')}
+      end
+    end   
   end
 end
